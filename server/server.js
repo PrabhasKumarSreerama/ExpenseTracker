c0f5fd3294171faf0d3fe3e4cbb14c6da559b114
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-27 19:46:43 +0530

    connected to DB

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const PORT = 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello EverOne!");
+});
+
+await mongoose.connect(
+  "mongodb+srv://sreeramaprabhaskumar:expensetracker@cluster0.wdbhiqx.mongodb.net/?retryWrites=true&w=majority"
+);
+console.log("MongoDB connection is successful");
+
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:4000");
+});
