81e42057e1334065106a1efc321103466620cc59
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-27 23:56:48 +0530

    body-parser for processing the json req data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const PORT = 4000;
 
@@ -9,12 +9,15 @@ const app = express();
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello EverOne!");
 });
 
-app.get("/transaction", (req, res) => {
-  res.send("Hello World!");
+app.post("/transaction", (req, res) => {
+  const { amount, description, date } = req.body;
+  res.json({ message: "Hello World!" });
 });
 
 await mongoose.connect(
