8b52b911d859b9037e1e66951f0d7755fcb39477
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2023-06-28 16:01:36 +0530

    fetch all transactions using default fetch(get)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [form, setForm] = useState({
@@ -6,6 +6,18 @@ function App() {
     description: "",
     date: "",
   });
+
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    fetchTransactionsHandler();
+  });
+
+  const fetchTransactionsHandler = async () => {
+    const res = await fetch("http://localhost:4000/transaction");
+    const { data } = await res.json();
+    setTransactions(data);
+  };
 
   const handleInputHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -20,8 +32,9 @@ function App() {
         "content-type": "application/json",
       },
     });
-    const data = await res.json();
-    console.log(data);
+    if (res.ok) {
+      fetchTransactionsHandler();
+    }
   };
 
   return (
@@ -49,6 +62,27 @@ function App() {
         />
         <button type="submit">Submit</button>
       </form>
+
+      <br />
+
+      <section>
+        <table>
+          <thead>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+          </thead>
+          <tbody>
+            {transactions.map((txn) => (
+              <tr key={txn._id}>
+                <td>{txn.amount}</td>
+                <td>{txn.description}</td>
+                <td>{txn.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
